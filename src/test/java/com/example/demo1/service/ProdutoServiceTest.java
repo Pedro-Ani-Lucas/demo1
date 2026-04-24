60f8879826de00a19f4a81ab9c3a9c1ce611baa1
PEDRO LUCAS PEREIRA DA SILVA <202451067401@alunos.newtonpaiva.edu.br>
2026-04-23 22:01:34 -0300

    Adicionando método de teste para validar preço

--- src/test/java/com/example/demo1/service/ProdutoServiceTest.java
+++ src/test/java/com/example/demo1/service/ProdutoServiceTest.java
@@ -1,9 +1,10 @@
 package com.example.demo1.service;
 
+import com.example.demo1.service.ProdutoService;
 import com.example.demo1.model.ProdutoEntity;
 import com.example.demo1.repository.ProdutoRepository;
-import com.example.demo1.service.ProdutoService;
 import org.junit.jupiter.api.Test;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -29,4 +30,25 @@ public class ProdutoServiceTest {
             assertEquals("Nome do produto é obrigatório", e.getMessage());
         }
     }
+
+    @Test
+    public void naoDeveSalvarProdutoComValorMenorQueZero() {
+
+        ProdutoRepository repository = mock(ProdutoRepository.class);
+        ProdutoService service = new ProdutoService(repository);
+
+        ProdutoEntity produto = new ProdutoEntity();
+        produto.setNome("Maçã");
+        produto.setPreco(-2.0); // inválido
+
+        try {
+            service.salvar(produto);
+            // se chegou aqui, o teste deve falhar
+            fail("Deveria lançar exceção");
+        } catch (IllegalArgumentException e) {
+            // valida mensagem
+            assertEquals("Preço inválido", e.getMessage());
+        }
+    }
+
 }
